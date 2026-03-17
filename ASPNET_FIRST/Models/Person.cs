6c928095c44f6974544be5beef93b9cc8fe48f20
maer1l <arseniy.istomin888@gmail.com>
2026-03-17 19:45:53 +0300

    n

---
+++ ASPNET_FIRST/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPNET_FIRST.Models
+{
+    public class Person
+    {
+        public double hello { get; set; }
+        public decimal world { get; set; }
+        public string lll { get; set; }
+    }
+}
