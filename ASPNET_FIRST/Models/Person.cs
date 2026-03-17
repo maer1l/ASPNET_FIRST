99ac19802da564eb35225427cfd534f2e4111fd4
maer1l <arseniy.istomin888@gmail.com>
2026-03-17 20:02:43 +0300

    added salary

--- ASPNET_FIRST/Models/Person.cs
+++ ASPNET_FIRST/Models/Person.cs
@@ -2,14 +2,15 @@
 {
     public class Person
     {
-        public Person(double hello, decimal world, string lll)
+        string hello;
+        string world;
+        string lll;
+        int salary;
+        public Person(string hello, string world, string lll)
         {
             this.hello = hello;
             this.world = world;
             this.lll = lll;
         }
-        public double hello { get; set; }
-        public decimal world { get; set; }
-        public string lll { get; set; }
     }
 }
